e96ecd6cf3ab65f6bf19d109dd6e97f5f8443c1c
Loh Zhang Yuan <jlohuk98@gmail.com>
2024-01-20 11:19:03 +0800

    added login component and modified app.js

---
+++ frontend/src/components/login.js
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Login = (props) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [usernameError, setUsernameError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+
+  const navigate = useNavigate();
+
+  const onButtonClick = () => {
+    setUsernameError("");
+    setPasswordError("");
+
+    if ("" === email) {
+      setUsernameError("Please enter your username");
+      return;
+    }
+
+    /* 
+    if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
+      setEmailError("Please enter a valid email");
+      return;
+    }
+    */
+
+    if ("" === password) {
+      setPasswordError("Please enter a password");
+      return;
+    }
+
+    if (password.length < 7) {
+      setPasswordError("The password must be 8 characters or longer");
+      return;
+    }
+
+    // Authentication calls will be made here...
+  };
+
+  return (
+    <div className={"mainContainer"}>
+      <div className={"titleContainer"}>
+        <div>Login</div>
+      </div>
+      <br />
+      <div className={"inputContainer"}>
+        <input
+          value={username}
+          placeholder="Enter your username here"
+          onChange={(ev) => setUsername(ev.target.value)}
+          className={"inputBox"}
+        />
+        <label className="errorLabel">{usernameError}</label>
+      </div>
+      <br />
+      <div className={"inputContainer"}>
+        <input
+          value={password}
+          placeholder="Enter your password here"
+          onChange={(ev) => setPassword(ev.target.value)}
+          className={"inputBox"}
+        />
+        <label className="errorLabel">{passwordError}</label>
+      </div>
+      <br />
+      <div className={"inputContainer"}>
+        <input
+          className={"inputButton"}
+          type="button"
+          onClick={onButtonClick}
+          value={"Log in"}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Login;
