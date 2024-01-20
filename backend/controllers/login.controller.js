38689f471936476607c5fbcb094762ecc0304764
Tej-Deep <ptejdeep@gmail.com>
2024-01-20 12:51:39 +0800

    Implemented JWT token and verification

--- backend/controllers/login.controller.js
+++ backend/controllers/login.controller.js
@@ -3,11 +3,13 @@ var mysql = require('mysql2');
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "Abh1@bh1",
+  password: "", // TODO: password
   database: "techtrek24"
 });
 
 var jwt = require("jsonwebtoken");
+
+const SECRET_KEY = process.env.JWT_SECRET_KEY
 
 // exports.login = 
 con.connect(function(err) {
@@ -15,19 +17,36 @@ con.connect(function(err) {
                     console.log("Connected!");
                 });
 
-// var usname = "johndoe"
 
 exports.login = (req, res) => {
+    console.log(req.body)
     con.query('select * from user where username = ?', [req.body.username], function (err, result) {
     if (err) throw err;
     console.log(result)
-    // console.log("Result: " + result[0].first_name);
     if(!result.length){
         return res.status(404).send({message: "User Not Found."})
     }
     if(result[0].password != req.body.password){
         return res.status(401).send({accessToken:null, message: "Invalid Password."})
     }
-    res.status(200).send({message: "Login Successful"})
+    const token = jwt.sign({user_id: result[0].id , username: result[0].username}, SECRET_KEY, {expiresIn: '1h'})
+    console.log(token)
+    res.status(200).send({jwttoken: token, message: "Login Successful"})
 });
 }
+
+exports.verifytoken = (req, res, next) =>{
+    const token = req.headers.auth;
+    if(!token){
+        return res.status(401).json({ message: 'Unauthorized - Token not provided' });
+    }
+
+    jwt.verify(token, SECRET_KEY, (err, decoded) => {
+        if (err) {
+          return res.status(401).json({ message: 'Unauthorized - Invalid token' });
+        }
+        console.log(decoded)
+        req.user_id = decoded.user_id;
+        next();
+      });
+};
--- backend/routes/login.js
+++ backend/routes/login.js
@@ -3,6 +3,9 @@ var router = express.Router();
 
 var logincontroller = require('../controllers/login.controller')
 
-router.get('/', logincontroller.login);
+router.post('/', logincontroller.login);
+// router.get('/protected', logincontroller.verifytoken, (req, res) => {
+//   res.json({ message: 'Protected route accessed', user: req.user });
+// });
 
 module.exports = router;
